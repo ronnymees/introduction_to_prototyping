0e885ae0c459f7c2186432b7142606f47ca43e71
Ronny Mees <ronny.mees@vives.be>
2024-01-18 12:40:22 +0100

    initial commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,8 @@ import { path } from '@vuepress/utils'
 
 module.exports = {
   lang: 'en-US',
-  title: 'Course Full Stack Development - Professional Bachelor Elektronica-ICT',
-  description: 'Course Full Stack Development',
+  title: 'Course Introduction to Prototyping - Professional Bachelor Elektronica-ICT',
+  description: 'Course Introduction to Prototyping',
   
   theme: defaultTheme({
     logo: '/files/afbeelding1.png',
@@ -17,88 +17,17 @@ module.exports = {
     ],
     sidebar: [
       {
-        text: 'Development Tools',
+        text: 'Prototyping Tools',
         collapsible: true,
         children: [
-          '/01_developmenttools/README.md',          
+          '/01_prototyping_tools/README.md',          
         ]
       },{
         text: 'Introduction',
         collapsible: true,
         children: [
-          '/11_history_and_evolution/README.md',
-          '/12_tcp_ip_stack/README.md',
-          '/13_client_server_model/README.md',
-          '/14_web_protocols/README.md',
-          '/15_internet_of_things/README.md',
+          '/11_introduction/README.md',          
         ]
-      },{
-        text: 'Frontend Webdesign',
-        collapsible: true,
-        children: [
-          '/21_folder_structure/README.md',
-          '/22_webstructure_html/README.md',
-          '/23_webstyling_css/README.md',
-          '/24_responsive_styling/README.md',
-          '/25_online_styles/README.md',
-          '/26_deploy_website/README.md',
-        ]
-      },{
-        text: 'Frontend Scripting',
-        collapsible: true,
-        children: [
-          '/31_communication/README.md',
-          '/32_frontend_scripting_js/README.md',
-          '/33_typescript/README.md',
-          '/34_advanced_js/README.md',
-        ]
-      },{
-        text: 'Backend databases',
-        collapsible: true,
-        children: [
-          '/41_databases/README.md',
-          '/42_setup_mysql/README.md',
-          '/43_setup_mariadb/README.md',
-          //'/44_install_phpmyadmin/README.md',
-          '/45_create_db_user/README.md',
-          '/46_crud_db/README.md',
-        ]
-      },{
-        text: 'Backend Scripting',
-        collapsible: true,
-        children: [
-          '/51_intro_express/README.md',
-          '/52_rest_api_crud/README.md',
-          '/53_rest_api_fileupload/README.md',
-          '/54_security/README.md',
-        ]
-      },{
-        text: 'Frontend Development',
-        collapsible: true,
-        children: [
-          '/61_frontend_vue/README.md',
-          '/66_routes/README.md',
-          '/62_restfull_apis/README.md',
-          '/63_crud/README.md',
-          '/64_axios/README.md',
-          '/65_vuetify/README.md',
-        ]
-      },{
-        text: 'Privacy regulations',
-        collapsible: true,
-        children: [
-          '/73_privacy/README.md',
-          //'/72_login/README.md',
-          //'/71_secure_api/README.md',          
-        ],
-      },{
-        text: 'Deployment',
-        collapsible: true,
-        children: [
-          '/81_docker/README.md',
-          //'/72_login/README.md',
-          //'/71_secure_api/README.md',          
-        ],
       }
     ],
     sidebarDepth: 1,
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,4 @@
-# Full Stack Webdevelopment
-
-![Netlify Status](https://api.netlify.com/api/v1/badges/2da24422-0bbe-45a8-a291-54c5d790d80f/deploy-status)
+# Introduction to Prototyping
 
 This course is still in development.
 
@@ -9,46 +7,22 @@ This course is still in development.
 ## Introduction
 
 This course is developed for PBA Electronics-ICT at [University College VIVES](https://www.vives.be/nl/technology/elektronica-ict).
-During this course you will learn the basics of HTML5, CSS3, Javascript, Typescript, Node.js, Express.js, Vue, MySQL, Docker, ... all tools to become a full stack developer.
+During this course you will learn ...
 
 ## Content
 
-1. Introduction to web development
-2. Frontend webdesign with HTML and CSS
-3. Frontend scripting with Javascript
-4. Backend databases with SQL
-5. Backend scripting with Express.js
-6. Frontend development
-7. Privacy regulations
-8. Deployment
+1. ...
 
 ## Sources
 
 This work is the result of the knowledge of the writer & colleagues at VIVES University college:
 
 * Ronny Mees
-* Nico De Witte
-* Sille Van Landschoot
+* Karolien Vandersickel
 
 And professionally relevant sources such as:
 
-* W3Schools ( let op met deze bron, is niet up to date en bevat veel fouten)
-* LinkedIn Learning
-* Youtube tutorials
-* Ferguson, R. (2019). Beginning JavaScript: The Ultimate Guide to Modern JavaScript Development.
-* Mozilla developer pagina
-* De github cursus van docent Rogier Van der Linde aan de hogeschool Odisee.
-* Blog van Matteo Duò op Kinsta i.v.m. responsive webdesign.
-* Blog van Michael J. Fordham op editorx i.v.m. cross-browser webdesign
-* Jon Duckett (2011). HTML & CSS: Design and build websites
-* Jon Duckett (2014). Javascript & Jquery: Interactive front-end web development
-* Jon Duckett (2022). PHP & MySQL: Server-side web development
-* Bezkoder.com
-* Greg Lim (2022). Beginning Vue3 Development
-* Heitor Ramon Ribeiro (2020). Vue.js 3 Cookbook
-* Maya Shavin & Raymond Camden (2023). Frontend Development Projects with Vue.js 3
-* DigitalOcean tutorials
-* Code for Geek
+* ...
 
 ## License
 
